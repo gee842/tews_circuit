366a2a765884e45127d4d7cb6b634b9f0dfc7050
Yap Jia Hong <yapjiahong2003@gmail.com>
2023-08-24 11:02:21 +0800

    chore(unused): constants

--- src/constants.rs
+++ src/constants.rs
@@ -1,9 +1,3 @@
 // User ids
 pub const ALPHABETS: u64 = 275797064674312193;
 pub const ALPHACOMS: u64 = 1135808742785556512;
-
-// Voice channels
-/// test channel from test server
-pub const TEST_GENERAL: &str = "976790417058168836";
-/// Voice channel named "Public" in Tews  channel from test server
-pub const TEWS_PUBLIC: &str = "976790417058168836";
