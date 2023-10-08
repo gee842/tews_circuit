304d205a616ca1b9b53d4d70241f3c3fb51fe82b
Yap Jia Hong <yapjiahong2003@gmail.com>
2023-10-08 17:27:10 +0800

    wip(chal menu): Working on challenge menu

--- challenge.py
+++ challenge.py
@@ -1,8 +1,9 @@
+from typing import Tuple
 import discord
 
-from views import UserSelect
+from views import UserSelect, Calendar
 
-from discord import app_commands
+from discord import app_commands, ChannelType
 from discord.ext import commands
 
 class Challenge(commands.Cog):
@@ -11,5 +12,50 @@ class Challenge(commands.Cog):
 
     @app_commands.command(name="challenge", description="Challenge someone.")
     async def challenge(self, interaction: discord.Interaction) -> None:
+        channel = interaction.channel
+        if channel is None:
+            return
+
+        if channel.type == ChannelType.category or channel.type == ChannelType.forum:
+            return
+
+        username = await self.user_selection(interaction)
+        if username is None:
+            return
+
+        await channel.send(f"You wish to challenge {username.global_name}? Good luck!")
+
+        # TODO: FIX: everything below here isn't working.
+        date = await self.date_selection(interaction)
+        if date is None:
+            return
+
+        (day, month) = date
+        await channel.send(f"The challenge is on {day} {month}!")
+
+    async def user_selection(self, interaction: discord.Interaction) -> discord.User | None:
         user_select = UserSelect()
         await interaction.response.send_message(view=user_select)
+
+        await user_select.wait()
+
+        username = user_select.user
+
+        if username is None:
+            return
+
+        username = username[0]
+        return username
+
+    async def date_selection(self, interaction: discord.Interaction) -> Tuple[str, dict[str, str]] | None:
+        calendar = Calendar()
+        await interaction.response.send_message(view=calendar)
+        await calendar.wait()
+
+        day = calendar.day
+        month = calendar.month
+
+        if day is None or month is None:
+            return
+
+        return (day, month)
--- views.py
+++ views.py
@@ -1,8 +1,37 @@
-from discord import ui 
+from typing import Dict
+import discord
+from discord import ui
+from discord.ui import Select, View, select
 
-class UserSelect(ui.View):
+class UserSelect(View):
     user = None
 
-    @ui.select(cls=ui.UserSelect)
-    async def challenge_user(self, interaction, select_item):
-        self.answer1 = select_item.values
+    @select(cls=ui.UserSelect, placeholder="Select a user")
+    async def challenge_user(self, interaction: discord.Interaction, select_item: Select):
+        self.user = select_item.values
+
+        await interaction.response.defer()
+        self.stop()
+
+class Calendar(ui.View):
+    day: str | None = None
+    days = [discord.SelectOption(label=str(i), value=str(i)) for i in range(1, 32)]
+
+    month: Dict[str, str] | None = None
+
+    month_names = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+    months = [discord.SelectOption(label=month, value=month[:3]) for month in month_names]
+
+    @ui.select(options = months, placeholder="Select the month of the challenge.")
+    async def select_month(self, interaction, select_item):
+        month = select_item.values
+        await interaction.response.defer()
+
+        self.stop()
+
+    @ui.select(options = days, placeholder="Select the day of the challenge.")
+    async def select_day(self, interaction, select_item):
+        day = select_item.values
+        await interaction.response.defer()
+
+        self.stop()
