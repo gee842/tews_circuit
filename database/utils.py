29e61574521c6835550186c913c87e34315cead2
Yap Jia Hong <yapjiahong2003@gmail.com>
2023-10-16 16:56:05 +0800

    fix(rank): Not added on db creation
    Before if the database was found to not exist, it would create the
    tables but not the ranks in the Ranks table. The fix patches this up.

--- database/utils.py
+++ database/utils.py
@@ -2,18 +2,21 @@ import aiosqlite
 from aiosqlite import Cursor
 
 async def verify_database():
-    async with aiosqlite.connect("database.db") as db:
-        async with db.execute("SELECT name FROM sqlite_master") as cursor:
-            tables = await cursor.fetchall()
-            num_tables = len(tables) # type: ignore
+    async with asqlite.connect("database.db") as db:
+        async with db.cursor() as cursor:
+            results = await cursor.execute("SELECT name FROM sqlite_master")
+            tables = await results.fetchall()
+            num_tables = len(tables)  # type: ignore
 
+            # true if db doesn't exist or has missing tables.
             if num_tables == 0 or num_tables < 5:
                 await execute_from_file(cursor, "sql/creation.sql")
+                await execute_from_file(cursor, "sql/insert_ranks.sql")
             elif num_tables == 5:
                 ranks = await cursor.execute("SELECT * FROM Ranks")
                 ranks = await ranks.fetchall()
 
-                if len(ranks) < 4: # type: ignore
+                if len(ranks) < 4:  # type: ignore
                     await execute_from_file(cursor, "sql/insert_ranks.sql")
 
 
